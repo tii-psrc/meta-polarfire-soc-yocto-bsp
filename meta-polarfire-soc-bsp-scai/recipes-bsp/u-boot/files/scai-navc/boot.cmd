a35037cffe8664ef4e8a4a7528a926e63e883657
kwangsu-unikie <kwangsu.jung@unikie.com>
2025-10-28 07:44:08 +0000

    The spiX numbering changed because QSPI controller is only enabled in linux kernel's device tree. Booting command for loading fitImage is changed.

--- meta-polarfire-soc-bsp-scai/recipes-bsp/u-boot/files/scai-navc/boot.cmd
+++ meta-polarfire-soc-bsp-scai/recipes-bsp/u-boot/files/scai-navc/boot.cmd
@@ -1,7 +1,8 @@
 
 ubifsload 0x80000000 boot/fitImage
 cp 0x80000000 ${scriptaddr} ${filesize}
-bootm start ${scriptaddr}#conf-microchip_mpfs-icicle-kit.dtb#conf-mpfs_icicle_flash5_click.dtbo
+#bootm start ${scriptaddr}#conf-microchip_mpfs-icicle-kit.dtb#conf-mpfs_icicle_flash5_click.dtbo
+bootm start ${scriptaddr}
 bootm loados ${scriptaddr};
 # Try to load a ramdisk if available inside fitImage
 bootm ramdisk;
