e62c296999c8c38f4ce5fa3d1d49af35038accce
kwangsu-unikie <kwangsu.jung@unikie.com>
2026-08-16 09:48:43 +0000

    scai_tm_test: boot_count filed is added.
    To send boot count data over telemetry, boot_count filed is added to telemetry data struture.
    Boot count is managed by three Systemd services  == scai-boot-count-X.service (0:rootfs, 1:scai_mtd_a, 2:scai_mtd_b)
    When boot couting is completed it configure the status file on /run/boot-count/FOO with "ready". If the boot count waits the mount, it configure different data.
    So, it is valid only the "READY" keyword exist.
    
    It is very difficult and dangerous to make this logic in Linux kernel driver.

--- meta-polarfire-soc-bsp-scai/recipes-apps/scai-tm-test/files/scai_tm_test.c
+++ meta-polarfire-soc-bsp-scai/recipes-apps/scai-tm-test/files/scai_tm_test.c
@@ -5,8 +5,27 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <stdint.h>
 
 #define TM_BUFFER_SIZE     (16 * 1024 * 1024)
+
+enum device_id {
+	NOM_MSS_W25 = 0,
+	NOM_FPGA_W25,
+	RED_MSS_W25,
+	RED_FPGA_W25,
+	BOOT_DEVICE_MAX
+};
+
+#define BOOT_COUNT_STATUS_DIR              "/run/boot-count"
+struct boot_count_w25 {
+	uint8_t boot_device[BOOT_DEVICE_MAX];
+};
+
+struct boot_count_ext {
+	uint8_t boot_device[BOOT_DEVICE_MAX];
+	uint8_t last_device;
+};
 
 struct telemetry_data {
 	int32_t temp_K;
@@ -51,6 +70,9 @@ struct telemetry_data {
 	int32_t sanity_check_sddr_vtt;
 	int32_t sanity_check_fddr_vtt;
 	int32_t edac_cnt_ddrc;
+	struct boot_count_w25 boot_count_w25;     // w25 -- main booting rootfs
+	struct boot_count_ext boot_count_mtd[2];  // [0] == scai_mtd_a, in case of dpu460 == 3dplus_mtd_a
+	                                          // [1] == scai_mtd_b, in case of dpu460 == 3dplus_mtd_b
 };
 
 struct user_data {
@@ -153,8 +175,123 @@ static void print_tm_data(struct telemetry_data *tm_data)
 			tm_data->sanity_check_sddr_vtt);
 	printf("tm_data->sanity_check_fddr_vtt         : %d\r\n",
 			tm_data->sanity_check_fddr_vtt);
+
 	printf("tm_data->edac_cnt_ddrc                 : %d\r\n",
 			tm_data->edac_cnt_ddrc);
+
+	printf("tm_data->boot_count_w25.boot_device[NOM_MSS_W25]     : 0x%02X\r\n",
+			tm_data->boot_count_w25.boot_device[NOM_MSS_W25]);
+	printf("tm_data->boot_count_w25.boot_device[NOM_FPGA_W25]    : 0x%02X\r\n",
+			tm_data->boot_count_w25.boot_device[NOM_FPGA_W25]);
+	printf("tm_data->boot_count_w25.boot_device[RED_MSS_W25]     : 0x%02X\r\n",
+			tm_data->boot_count_w25.boot_device[RED_MSS_W25]);
+	printf("tm_data->boot_count_w25.boot_device[RED_FPGA_W25]    : 0x%02X\r\n",
+			tm_data->boot_count_w25.boot_device[RED_FPGA_W25]);
+
+	printf("tm_data->boot_count_mtd[0].boot_device[NOM_MSS_W25]  : 0x%02X\r\n",
+			tm_data->boot_count_mtd[0].boot_device[NOM_MSS_W25]);
+	printf("tm_data->boot_count_mtd[0].boot_device[NOM_FPGA_W25] : 0x%02X\r\n",
+			tm_data->boot_count_mtd[0].boot_device[NOM_FPGA_W25]);
+	printf("tm_data->boot_count_mtd[0].boot_device[RED_MSS_W25]  : 0x%02X\r\n",
+			tm_data->boot_count_mtd[0].boot_device[RED_MSS_W25]);
+	printf("tm_data->boot_count_mtd[0].boot_device[RED_FPGA_W25] : 0x%02X\r\n",
+			tm_data->boot_count_mtd[0].boot_device[RED_FPGA_W25]);
+	printf("tm_data->boot_count_mtd[0].last_device               : 0x%02X\r\n",
+			tm_data->boot_count_mtd[0].last_device);
+
+	printf("tm_data->boot_count_mtd[1].boot_device[NOM_MSS_W25]  : 0x%02X\r\n",
+			tm_data->boot_count_mtd[1].boot_device[NOM_MSS_W25]);
+	printf("tm_data->boot_count_mtd[1].boot_device[NOM_FPGA_W25] : 0x%02X\r\n",
+			tm_data->boot_count_mtd[1].boot_device[NOM_FPGA_W25]);
+	printf("tm_data->boot_count_mtd[1].boot_device[RED_MSS_W25]  : 0x%02X\r\n",
+			tm_data->boot_count_mtd[1].boot_device[RED_MSS_W25]);
+	printf("tm_data->boot_count_mtd[1].boot_device[RED_FPGA_W25] : 0x%02X\r\n",
+			tm_data->boot_count_mtd[1].boot_device[RED_FPGA_W25]);
+	printf("tm_data->boot_count_mtd[1].last_device               : 0x%02X\r\n",
+			tm_data->boot_count_mtd[1].last_device);
+}
+
+struct boot_count_entry {
+	const char *status_file;
+	const char *boot_count_file;
+	void *boot_count;
+	size_t boot_count_size;
+};
+
+int set_boot_count(struct telemetry_data *p_tm_data)
+{
+	char buf[256];
+
+	struct boot_count_entry entries[] = {
+		{
+			"/run/boot-count/w25.status",
+			"/boot_count",
+			&p_tm_data->boot_count_w25,
+			sizeof(p_tm_data->boot_count_w25)
+		},
+		{
+			"/run/boot-count/mtd_a.status",
+			"/opt/scai_mtd_a/boot_count",
+			&p_tm_data->boot_count_mtd[0],
+			sizeof(p_tm_data->boot_count_mtd[0])
+		},
+		{
+			"/run/boot-count/mtd_b.status",
+			"/opt/scai_mtd_b/boot_count",
+			&p_tm_data->boot_count_mtd[1],
+			sizeof(p_tm_data->boot_count_mtd[1])
+		},
+	};
+
+	for (size_t i = 0; i < sizeof(entries) / sizeof(entries[0]); i++) {
+		int fd;
+		ssize_t n;
+
+		fd = open(entries[i].status_file, O_RDONLY);
+		if (fd < 0) {
+			perror(entries[i].status_file);
+			continue;
+		}
+
+		n = read(fd, buf, sizeof(buf) - 1);
+		close(fd);
+
+		if (n < 0) {
+			perror("read status");
+			continue;
+		}
+
+		buf[n] = '\0';
+
+		if (strstr(buf, "READY") == NULL)
+			continue;
+
+		fd = open(entries[i].boot_count_file, O_RDONLY);
+		if (fd < 0) {
+			perror(entries[i].boot_count_file);
+			continue;
+		}
+
+		n = read(fd,
+				entries[i].boot_count,
+				entries[i].boot_count_size);
+
+		close(fd);
+
+		if (n != (ssize_t)entries[i].boot_count_size) {
+			if (n < 0)
+				perror("read boot_count");
+			else
+				fprintf(stderr,
+						"%s: invalid size %zd\n",
+						entries[i].boot_count_file,
+						n);
+
+			continue;
+		}
+	}
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -174,10 +311,11 @@ int main(int argc, char *argv[])
 				"    0 - concise output\n"
 				"    1 - verbose output\n"
 				"    2 - stop HSS services\n",
+				argv[0]);
+		fprintf(stderr,
 				"  stop_service(if mode == 2)\n"
 				"    1 - stop telemetry publishing via UART @HSS\n"
-				"    2 - stop external watchdog pining @HSS\n",
-				argv[0]);
+				"    2 - stop external watchdog pining @HSS\n");
 		return EXIT_FAILURE;
 	}
 
@@ -185,7 +323,7 @@ int main(int argc, char *argv[])
 	user_data.arg0 = strtoul(argv[2], NULL, 0);
 	if (user_data.arg0 < 0 || user_data.arg0 > 2) {
 		fprintf(stderr,
-				"Invalid mode: %d\n"
+				"Invalid mode: %ld\n"
 				"Supported modes:\n"
 				"  0 - concise\n"
 				"  1 - verbose\n"
@@ -206,7 +344,7 @@ int main(int argc, char *argv[])
 		user_data.size = strtol(argv[3], NULL, 0);
 		if (user_data.size < 1 || user_data.size > 2) {
 			fprintf(stderr,
-					"Invalid stop_service list: %d\n"
+					"Invalid stop_service list: %ld\n"
 					"Supported stop services:\n"
 					"    1 - stop telemetry publishing via UART @HSS\n"
 					"    2 - stop external watchdog pining @HSS\n",
@@ -250,6 +388,7 @@ int main(int argc, char *argv[])
 		printf("%s", buffer);
 	} else if (user_data.arg0 == SBI_TM_EXT_CONCISE) {
 		p_tm_data = (struct telemetry_data *)user_data.buf;
+		set_boot_count(p_tm_data);
 		print_tm_data(p_tm_data);
 	}
 	printf("\n----------------------------------------\n");
